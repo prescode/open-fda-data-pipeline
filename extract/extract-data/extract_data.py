a133a646183131b3e8ddc34f63db28280c7735af
prescode <deanbuilding@gmail.com>
2020-12-07 21:37:01 -0700

    Successfully extracted data

--- extract/extract-data/extract_data.py
+++ extract/extract-data/extract_data.py
@@ -1,15 +1,16 @@
 import json
 import boto3
 import botocore
-import requests
 import os
+import urllib.request
 from botocore.client import ClientError
 
-S3_TARGET_BUCKET = os.environ['fda-data-raw']
+S3_TARGET_BUCKET = os.environ['S3_TARGET_BUCKET']
+#for local testing
+#S3_TARGET_BUCKET = 'fda-data-raw'
 
 def write_file_to_s3(file_name, bucket, object_name = None):
     # If S3 object_name was not specified, use file_name
-    print(type(file_name))
     if object_name is None:
         object_name = file_name
     # Upload the file
@@ -23,15 +24,17 @@ def write_file_to_s3(file_name, bucket, object_name = None):
 
 def lambda_handler(event, context):
     for record in event['Records']:
-        bucket = record['s3']['bucket']['name']
+        bucket_name = record['s3']['bucket']['name']
         key = record['s3']['object']['key']
-        print('s3 bucket: ' + bucket + '_s3 key: ' + key)
-        s3 = boto3.resource('s3')
-        obj = s3.Object(bucket, key)
-        url = obj.get()['url'].read().decode('utf-8')
-        r = requests.get(url, allow_redirects=True)
-        open(key, 'w').write(r.content)
-        write_file_to_s3(key, S3_TARGET_BUCKET)
-            
-
-            
+        print('s3 bucket: ' + bucket_name + '_s3 key: ' + key)
+        s3 = boto3.client('s3')
+        response = s3.get_object(Bucket=bucket_name, Key=key)
+        body = response['Body'].read()
+        url = json.loads(body)['url']
+        year = url.split('/')[-2][:-2]
+        file_name =  url.split('/')[-1]
+        if not os.path.exists(year):
+            os.makedirs(year)
+        new_file_name = year + "/" + file_name
+        urllib.request.urlretrieve(url, new_file_name)
+        write_file_to_s3(new_file_name, S3_TARGET_BUCKET)
