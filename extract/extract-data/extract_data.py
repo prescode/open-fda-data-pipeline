e508ac72343a316c554ac974de8f2c0c54b6d62e
prescode <deanbuilding@gmail.com>
2020-12-09 12:21:00 -0700

    Updated to memory stream file to s3

--- extract/extract-data/extract_data.py
+++ extract/extract-data/extract_data.py
@@ -2,21 +2,19 @@ import json
 import boto3
 import botocore
 import os
-import urllib.request
+from urllib.request import urlopen
+from io import BytesIO
 from botocore.client import ClientError
 
 S3_TARGET_BUCKET = os.environ['s3_target_bucket']
 #for local testing
 #S3_TARGET_BUCKET = 'fda-data-raw'
 
-def write_file_to_s3(file_name, bucket, object_name = None):
-    # If S3 object_name was not specified, use file_name
-    if object_name is None:
-        object_name = file_name
-    # Upload the file
+def write_object_to_s3(obj, bucket, object_name):
+    # Upload the object
     s3_client = boto3.client('s3')
     try:
-        s3_client.upload_file(file_name, bucket, object_name)
+        s3_client.put_object(obj, bucket, object_name)
     except ClientError as e:
         print(e)
         return False
@@ -34,6 +32,5 @@ def lambda_handler(event, context):
         year = url.split('/')[-2][:-2]
         file_name =  url.split('/')[-1]
         new_file_path = year + "/" + file_name
-        temp_file_path = '/tmp/temp.zip'
-        urllib.request.urlretrieve(url, temp_file_path)
-        write_file_to_s3(temp_file_path, S3_TARGET_BUCKET, new_file_path)
+        with urlopen(url) as zip_response:
+            write_object_to_s3(BytesIO(zip_response.read()), S3_TARGET_BUCKET, new_file_path)
