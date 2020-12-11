f19783fc058f29eb438e07b31e3490654bae569b
prescode <deanbuilding@gmail.com>
2020-12-11 14:50:53 -0700

    Loading files from S3

--- load/load.py
+++ load/load.py
@@ -1,7 +1,8 @@
 import json
 import os
-import uuid
 import boto3
+from io import BytesIO
+import zipfile
 from elasticsearch import Elasticsearch, RequestsHttpConnection
 from requests_aws4auth import AWS4Auth 
 from elasticsearch.helpers import streaming_bulk
@@ -26,20 +27,32 @@ es = Elasticsearch(
     )
 
 def lambda_handler(event, context):
-    #create index with mappings
-    mappings = json.load(open('elasticsearch_mapping.json'))
-    es.indices.create(index=ES_INDEX_NAME, body=mappings, ignore=400)
-    successes = 0
-    for ok, action in streaming_bulk(
-        client = es, index=ES_INDEX_NAME, actions=generate_docs(),
-    ):
-        successes += ok
-    print("Indexed %d documents" % (successes))
+    for record in event['Records']:
+        bucket_name = record['s3']['bucket']['name']
+        key = record['s3']['object']['key']
+        print('s3 bucket: ' + bucket_name + ' key: ' + key)
+        file_name = key.split('/')[-1]
+        #remove .zip for the new filename
+        extracted_file_name = os.path.splitext(file_name)[0]
+        s3 = boto3.client('s3')
+        s3_response_object = s3.get_object(Bucket=bucket_name, Key=key)
+        zip_object = BytesIO(s3_response_object['Body'].read())
+        #extract zip stream
+        with zipfile.ZipFile(zip_object) as zip_archive:
+            extracted_data = zip_archive.read(extracted_file_name)
+        #extract results (remove metadata header)
+        data = json.loads(extracted_data)
+        #create index with mappings
+        mappings = json.load(open('elasticsearch_mapping.json'))
+        #try create each time (will fail if already exists but be ignored)
+        es.indices.create(index=ES_INDEX_NAME, body=mappings, ignore=400)
+        successes = 0
+        for ok, action in streaming_bulk(
+            client = es, index=ES_INDEX_NAME, actions=generate_docs(data),
+        ):
+            successes += ok
+        print("Indexed %d documents" % (successes))
 
-def generate_docs():
-    data = json.load(open('sample_data.json'))
+def generate_docs(data):
     for d in data:
         yield d
-
-
-
