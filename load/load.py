5f853148c9938c861d4095dc88bd1c808abb9b44
prescode <deanbuilding@gmail.com>
2020-12-11 15:00:07 -0700

    Prepare for deploy to AWS

--- load/load.py
+++ load/load.py
@@ -7,12 +7,12 @@ from elasticsearch import Elasticsearch, RequestsHttpConnection
 from requests_aws4auth import AWS4Auth 
 from elasticsearch.helpers import streaming_bulk
 
-#ES_INDEX_NAME = os.environ['index_name']
-ES_INDEX_NAME = 'test-index'
-#ES_HOST = os.environ['host']
-ES_HOST = 'vpc-fda-data-c2lwohh63nlfnhb4csbexcxt5a.us-east-1.es.amazonaws.com' # For example, my-test-domain.us-east-1.es.amazonaws.com
-#ES_REGION = os.environ['host_region']
-ES_REGION= 'us-east-1'
+ES_INDEX_NAME = os.environ['index_name']
+#ES_INDEX_NAME = 'test-index'
+ES_HOST = os.environ['host']
+#ES_HOST = 'vpc-fda-data-c2lwohh63nlfnhb4csbexcxt5a.us-east-1.es.amazonaws.com' # For example, my-test-domain.us-east-1.es.amazonaws.com
+ES_REGION = os.environ['host_region']
+#ES_REGION= 'us-east-1'
 
 service = 'es'
 credentials = boto3.Session().get_credentials()
